2d5776027842dfb06a92b43c25850f652aa9fe17
Bryan Haney <i.am.bryan.haney@gmail.com>
2020-03-02 16:48:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -2,6 +2,11 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  i = 0
+  while i < collection.length do
+    return collection[i] if collection[i][:item] == name
+    i += 1
+  end
 end
 
 def consolidate_cart(cart)
@@ -9,12 +14,43 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  con_cart = []
+  i = 0
+  while i < cart.length do
+    item = find_item_by_name_in_collection(cart[i][:item], con_cart)
+    if item
+      item[:count] += 1
+    else
+      item = cart[i].clone
+      item[:count] = 1
+      con_cart << item
+    end
+    i += 1
+  end
+  con_cart
 end
 
 def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  
+  puts "\n\nPretty Printing Cart *************\n\n"
+  pp cart
+  puts "\n\nPretty Printing Coupons ***********\n\n"
+  pp coupons
+  puts "\n"
+  
+  i = 0
+  while i < coupons.length do
+    item = find_item_by_name_in_collection(coupon[i][:item], cart)
+    if (item && item[:count] >= 2)
+      item_with_coupon = item.clone
+      item_with_coupon[:count] = coupon[i][:num]
+      item_with_coupon[:price] = (coupon[i][:cost] / coupon[i][:count]).round(2)
+      
+    
+    
 end
 
 def apply_clearance(cart)
